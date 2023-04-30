043014d7462492208217d468b6c079e3ce09c362
prettyturtle <unifconti@gmail.com>
2023-05-01 01:35:23 +0900

    Initial Commit

---
+++ Swift_30_Projects_FacebookMe/Swift_30_Projects_FacebookMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_FacebookMeApp.swift
+//  Swift_30_Projects_FacebookMe
+//
+//  Created by yc on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_FacebookMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
